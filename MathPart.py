9c005acd566f0719f724a40cb9d691aa4a283fd3
JuneyBoy <arjun.ganesan@ou.edu>
2020-02-08 15:14:18 -0600

    Update MathPart.py
    Seperated functions

--- MathPart.py
+++ MathPart.py
@@ -1,19 +1,38 @@
 import yfinance as yf
-def stockPriceCalculator(name, initInvestment, investmentDate, currentDate):
 
-    initStockPrice = 0.00
+def stockPriceCalculator(name, initAmount, startDate, endDate):
     numOfStocks = 0
-    currentStockPrice = 100.0
-    currentValueOfInvestment = 0.0
-
-    numOfStocks = (int)(initInvestment/initStockPrice)
-
-    currentValueOfInvestment = numOfStocks * currentStockPrice
-
-    return currentValueOfInvestment
+    initInvestment = 0.00
+    initStockPrice = 0.00
+    endStockPrice = 0.00
+    endValueOfInvestment = 0.00
+    investmentGain = 0.00
 
 
+    initStockPrice = stockPriceLocator(name, startDate)
+    endStockPrice = stockPriceLocator(name, endDate)
 
-appleStock = yf.Ticker("AAPL")
+    numOfStocks = int(initAmount/initStockPrice)
+    print ("The value of the stock on " + startDate + " was $" + str(initStockPrice))
+    initInvestment = numOfStocks * initStockPrice
+    print("You were able to buy " + str(numOfStocks) + " stocks at $" + str(initInvestment))
 
-appleStock.info
+    endValueOfInvestment = round((numOfStocks * endStockPrice), 2)
+    print ("The value of the stock on " + endDate + " was $" + str(endStockPrice))
+    print("Your investment was worth $" + str(endValueOfInvestment) + " on " + endDate)
+
+    investmentGain = round((endValueOfInvestment - initInvestment), 2)
+    print("Your investment gained $" + str(investmentGain))
+
+def stockPriceLocator(name1, date):
+    stockName = yf.Ticker(name1)
+    stockPrice = 0.00
+
+    stockDataTable = stockName.history(start = date, end = date)
+    stockPrice = float(stockDataTable.loc[date, "Close"])
+    return stockPrice
+
+try:
+    stockPriceCalculator("FB", 1000, "2016-05-30", "2020-02-07")
+except:
+    pass
