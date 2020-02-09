61a9c051b395fc672c9612cafe30595a3085e53e
Benjamin Xia <bzijiexia@gmail.com>
2020-02-09 10:49:16 -0600

    Added Formatting

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, send_file, make_response, request
+from flask import render_template, send_file, make_response, request, redirect, url_for
 from app import app
 from flask_bootstrap import Bootstrap
 import MathPart as mp
@@ -9,18 +9,23 @@ import base64
 @app.route('/index', methods=['GET', 'POST'])
 def index():
     # pgain = mp.stockPriceCalculator("TSLA", 1000, "2020-02-03", "2020-02-07")
-    stock = 'Enter a Stock'
+    stock = ''
+    submitted = False
     if request.method == 'POST':
         stock = request.form.get('stock')
         init = request.form.get('init')
         start = request.form.get('start')
         end = request.form.get('end')
-        # mp.stockPriceCalculator(stock, init, start, end)
-
+        submitted = request.form.get('submitted')
+        if submitted == 'Try Again?':
+            submitted = False
+            return redirect(url_for('index'))
+        answers = mp.stockPriceCalculator(stock, float(init), start, end)
         bytes_obj = mp.stockPlotter(stock, start, end)
+        submitted = True
         # session['stock'] = stock
         plot_url = base64.b64encode(bytes_obj.getvalue()).decode()
         return render_template(
-            'index.html', title='Stock Tracker', stock=stock, plot_url=plot_url)
+            'index.html', stock=stock, plot_url=plot_url, submitted=submitted, answers=answers)
     return render_template(
-        'index.html', title='Stock Tracker', stock=stock)
+        'index.html', stock=stock, submitted=submitted)
