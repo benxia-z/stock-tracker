2f0421504f2efe0e9aa261f85908752d3de90e21
Benjamin Xia <bzijiexia@gmail.com>
2020-02-09 11:21:12 -0600

    Edit error

--- app/routes.py
+++ app/routes.py
@@ -11,16 +11,19 @@ def index():
     # pgain = mp.stockPriceCalculator("TSLA", 1000, "2020-02-03", "2020-02-07")
     stock = ''
     submitted = False
+    error = False
     if request.method == 'POST':
         stock = request.form.get('stock')
         init = request.form.get('init')
         start = request.form.get('start')
         end = request.form.get('end')
         submitted = request.form.get('submitted')
+        if not isStockReal(stock):
+            return render_template('index.html', stock=stock, plot_url=plot_url, submitted=submitted, answers=answers)
         if submitted == 'Try Again?':
             submitted = False
             return redirect(url_for('index'))
-        answers = mp.stockPriceCalculator(stock, float(init), start, end)
+        answers = mp.stockPriceCalculator(stock, round(float(init), 2), start, end)
         bytes_obj = mp.stockPlotter(stock, start, end)
         submitted = True
         # session['stock'] = stock
