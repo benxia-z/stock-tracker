b1cd0f4dbcb453e4b0a016e62ee30c74ebabc094
Benjamin Xia <bzijiexia@gmail.com>
2020-02-08 18:46:39 -0600

    Add Bootstrap

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,10 @@
 from flask import render_template
 from app import app
+from flask_bootstrap import Bootstrap
+import MathPart as mp
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title='Stock Tracker')
+    pgain = mp.stockPriceCalculator("TSLA", 1000, "2020-02-03", "2020-02-07")
+    return render_template('index.html', title='Stock Tracker', pgain=pgain)
