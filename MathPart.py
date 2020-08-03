63a5bb6f5afcfd974bb638c97142be28826b61d7
JuneyBoy <arjun.ganesan@ou.edu>
2020-08-02 23:49:08 -0500

    Created stock class to replace stockPriceCalculator function

--- MathPart.py
+++ MathPart.py
@@ -6,6 +6,72 @@ import io
 
 from datetime import datetime
 #matplotlib.use('agg')
+
+#class represents stock object with an initial investment amount and start and end dates of investment
+amazon_stock = Stock('AMZN', 1000, '2010-01-01', '2020-01-01')
+amazon_stock.info_print_out()
+info_print_out()
+
+class Stock:
+    
+    def __init__(self, stock_name, init_amount, start_date, end_date):
+        
+        self.stock_name = stock_name
+        self.init_amount = init_amount
+        self.start_date = start_date
+        self.end_date = end_date
+        
+        #gets price of stock on the start and end dates
+        self.init_stock_price = self.stock_price_locator(stock_name, start_date)
+        self.final_stock_price = self.stock_price_locator(stock_name, end_date)
+
+        #integer number of stocks you can buy
+        self.num_of_stocks = init_amount // self.init_stock_price
+        
+        #can buy stocks or not
+        self.can_buy_stock = True
+        if self.num_of_stocks <= 0:
+            self.can_buy_stock = False
+
+        self.init_investment_amount = round(self.num_of_stocks * self.init_stock_price, 2)
+        self.final_investment_amount = round(self.num_of_stocks * self.final_stock_price, 2)
+
+        self.investment_gain = round(self.final_investment_amount - self.init_investment_amount, 2)
+        self.percentage_gain = round((self.investment_gain / self.init_investment_amount) * 100, 2)
+
+    def stock_price_locator(self, stock, date):
+        stockPrice = 0.00
+        stockName = yf.Ticker(stock)
+        stockDataTable = stockName.history(start = date)
+        stockPrice = float(stockDataTable["Close"][0])
+        return stockPrice
+    
+    def info_print_out(self):
+        
+        if(self.investment_gain >= 0):
+            self.investment_gain_info = "Your investment gained $" + str(self.investment_gain)
+        else:
+            self.investment_gain_info = "Your investment lost $" + str(self.investment_gain)
+
+        if(self.investment_gain >= 0):
+            self.percentage_gain_info = "That's a  %" + self.percentage_gain + "gain !"
+        else:
+            self.percentage_gain_info = "That's a  %" + self.percentage_gain + "loss !"
+        
+        {'Start Info': "The value of " + self.stoc + " on " + self.start_date + " was $" + str(self.init_stock_price),
+         'Buy Info': "You were able to buy " + str(self.num_of_stocks) + " shares at $" + str(self.init_investment_amount),
+         'Final Info': "The value of " + self.stoc + " on " + self.end_date + " was $" + str(self.final_stock_price),
+         'End Investment Info': "The value of your investment on " + self.end_date + " was worth $" + str(self.final_investment_amount),
+         'Investment Gain info': self.investment_gain_info,
+         'Percentage Gain Info': self.percentage_gain_info}
+        
+        
+        
+        self.endStockPrice = 0.00
+        self.endValueOfInvestment = 0.00
+        self.investmentGain = 0.00
+        self.percentageGain = 0.00
+
 def stockPriceCalculator(firstStockName, secondStockName, thirdStockName, initAmount, startDate, endDate):
     numOfStocks1 = 0
     initInvestment = 0.00
@@ -29,14 +95,14 @@ def stockPriceCalculator(firstStockName, secondStockName, thirdStockName, initAm
     investmentGain3 = 0.00
     percentageGain3 = 0.00
 
-    initStockPrice1 = stockPriceLocator(firstStockName, startDate)
-    endStockPrice1 = stockPriceLocator(firstStockName, endDate)
+    initStockPrice1 = stock_price_locator(firstStockName, startDate)
+    endStockPrice1 = stock_price_locator(firstStockName, endDate)
 
-    initStockPrice2 = stockPriceLocator(secondStockName, startDate)
-    endStockPrice2 = stockPriceLocator(secondStockName, endDate)
+    initStockPrice2 = stock_price_locator(secondStockName, startDate)
+    endStockPrice2 = stock_price_locator(secondStockName, endDate)
 
-    initStockPrice3 = stockPriceLocator(thirdStockName, startDate)
-    endStockPrice3 = stockPriceLocator(thirdStockName, endDate)
+    initStockPrice3 = stock_price_locator(thirdStockName, startDate)
+    endStockPrice3 = stock_price_locator(thirdStockName, endDate)
 
     numOfStocks1 = int(initAmount/initStockPrice1)
     startInfo1 = "The value of the first stock on " + startDate + " was $" + str(initStockPrice1)
@@ -119,12 +185,7 @@ def stockPriceCalculator(firstStockName, secondStockName, thirdStockName, initAm
 
     return (returnInfo)
 
-def stockPriceLocator(stock, date):
-    stockPrice = 0.00
-    stockName = yf.Ticker(stock)
-    stockDataTable = stockName.history(start = date)
-    stockPrice = float(stockDataTable["Close"][0])
-    return stockPrice
+
 
 def stockPlotter(firstStockProg, secondStockProg, thirdStockProg, startDate2, endDate2): #Implement multiple names (name1, name2 and name3?)
     stockName1 = yf.Ticker(firstStockProg)
