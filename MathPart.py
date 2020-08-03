bdb92136cdefc29471f42f661eec26fdf0f16a95
JuneyBoy <arjun.ganesan@ou.edu>
2020-08-03 00:03:13 -0500

    Deleted StockPriceCalculator function

--- MathPart.py
+++ MathPart.py
@@ -58,9 +58,9 @@ class Stock:
         else:
             self.percentage_gain_info = "That's a  %" + self.percentage_gain + "loss !"
         
-        {'Start Info': "The value of " + self.stoc + " on " + self.start_date + " was $" + str(self.init_stock_price),
+        {'Start Info': "The value of " + self.stock_name + " on " + self.start_date + " was $" + str(self.init_stock_price),
          'Buy Info': "You were able to buy " + str(self.num_of_stocks) + " shares at $" + str(self.init_investment_amount),
-         'Final Info': "The value of " + self.stoc + " on " + self.end_date + " was $" + str(self.final_stock_price),
+         'Final Info': "The value of " + self.stock_name + " on " + self.end_date + " was $" + str(self.final_stock_price),
          'End Investment Info': "The value of your investment on " + self.end_date + " was worth $" + str(self.final_investment_amount),
          'Investment Gain info': self.investment_gain_info,
          'Percentage Gain Info': self.percentage_gain_info}
@@ -71,121 +71,6 @@ class Stock:
         self.endValueOfInvestment = 0.00
         self.investmentGain = 0.00
         self.percentageGain = 0.00
-
-def stockPriceCalculator(firstStockName, secondStockName, thirdStockName, initAmount, startDate, endDate):
-    numOfStocks1 = 0
-    initInvestment = 0.00
-    initStockPrice1 = 0.00
-    endStockPrice1 = 0.00
-    endValueOfInvestment1 = 0.00
-    investmentGain1 = 0.00
-    percentageGain1 = 0.00
-
-    numStocks2 = 0
-    initStockPrice2 = 0.00
-    endStockPrice2 = 0.00
-    endValueOfInvestment2 = 0.00
-    investmentGain2 = 0.00
-    percentageGain2 = 0.00
-
-    numStocks3 = 0
-    initStockPrice3 = 0.00
-    endStockPrice3 = 0.00
-    endValueOfInvestment3 = 0.00
-    investmentGain3 = 0.00
-    percentageGain3 = 0.00
-
-    initStockPrice1 = stock_price_locator(firstStockName, startDate)
-    endStockPrice1 = stock_price_locator(firstStockName, endDate)
-
-    initStockPrice2 = stock_price_locator(secondStockName, startDate)
-    endStockPrice2 = stock_price_locator(secondStockName, endDate)
-
-    initStockPrice3 = stock_price_locator(thirdStockName, startDate)
-    endStockPrice3 = stock_price_locator(thirdStockName, endDate)
-
-    numOfStocks1 = int(initAmount/initStockPrice1)
-    startInfo1 = "The value of the first stock on " + startDate + " was $" + str(initStockPrice1)
-
-    numOfStocks2 = int(initAmount/initStockPrice2)
-    startInfo2 = "The value of the second stock on " + startDate + " was $" + str(initStockPrice2)
-
-    numOfStocks3 = int(initAmount/initStockPrice3)
-    startInfo3 = "The value of the third stock on " + startDate + " was $" + str(initStockPrice3)
-
-    if (numOfStocks1 <= 0):
-        print("You were not able to buy any stocks for the first stock with your initial investment. Please enter a new amount: ")
-        return False
-
-    if (numOfStocks2 <= 0):
-        print("You were not able to buy any stocks for the second stock with your initial investment. Please enter a new amount: ")
-        return False
-
-    if (numOfStocks3 <= 0):
-        print("You were not able to buy any stocks for the third stock with your initial investment. Please enter a new amount: ")
-        return False
-
-    initInvestment = round((numOfStocks1 * initStockPrice1), 2)
-    buyInfo1 = "You were able to buy " + str(numOfStocks1) + " stocks at $" + str(initInvestment)
-
-    initInvestment = round((numOfStocks2 * initStockPrice2), 2)
-    buyInfo2 = "You were able to buy " + str(numOfStocks2) + " stocks at $" + str(initInvestment)
-
-    initInvestment = round((numOfStocks3 * initStockPrice3), 2)
-    buyInfo3 = "You were able to buy " + str(numOfStocks3) + " stocks at $" + str(initInvestment)
-
-    endValueOfInvestment1 = round((numOfStocks1 * endStockPrice1), 2)
-    endStockInfo1 = "The value of the first stock on " + endDate + " was $" + str(endStockPrice1)
-    endInvestmentInfo1 = "The value of your investment in the first stock on " + endDate + " was worth $" + str(endValueOfInvestment1)
-
-    endValueOfInvestment2 = round((numOfStocks2 * endStockPrice2), 2)
-    endStockInfo2 = "The value of the second stock on " + endDate + " was $" + str(endStockPrice2)
-    endInvestmentInfo2 = "The value of your investment in the second stock on " + endDate + " was worth $" + str(endValueOfInvestment2)
-
-    endValueOfInvestment3 = round((numOfStocks3 * endStockPrice3), 2)
-    endStockInfo3 = "The value of the third stock on " + endDate + " was $" + str(endStockPrice3)
-    endInvestmentInfo3 = "The value of your investment in the third stock on " + endDate + " was worth $" + str(endValueOfInvestment3)
-
-    investmentGain1 = round((endValueOfInvestment1 - initInvestment), 2)
-    percentageGain1 = round(((endValueOfInvestment1 - initInvestment)/initInvestment) * 100, 2)
-
-    investmentGain2 = round((endValueOfInvestment2 - initInvestment), 2)
-    percentageGain2 = round(((endValueOfInvestment2 - initInvestment)/initInvestment) * 100, 2)
-
-    investmentGain3 = round((endValueOfInvestment3 - initInvestment), 2)
-    percentageGain3 = round(((endValueOfInvestment3 - initInvestment)/initInvestment) * 100, 2)
-
-    if(investmentGain1 < 0):
-        investmentGainInfo1 = "Your first investment lost $" + str(investmentGain1)
-        percentageGaininfo1 = "That's a " + str(percentageGain1) + "% loss!"
-    else:
-        investmentGainInfo1 = "Your first investment gained $" + str(investmentGain1)
-        percentageGaininfo1 = "That's a " + str(percentageGain1) + "% gain!"
-
-    if(investmentGain2 < 0):
-        investmentGainInfo2 = "Your second investment lost $" + str(investmentGain2)
-        percentageGaininfo2 = "That's a " + str(percentageGain2) + "% loss!"
-    else:
-        investmentGainInfo2 = "Your second investment gained $" + str(investmentGain2)
-        percentageGaininfo2 = "That's a " + str(percentageGain2) + "% gain!"
-
-    if(investmentGain3 < 0):
-        investmentGainInfo3 = "Your third investment lost $" + str(investmentGain3)
-        percentageGaininfo3 = "That's a " + str(percentageGain3) + "% loss!"
-    else:
-        investmentGainInfo3 = "Your third investment gained $" + str(investmentGain3)
-        percentageGaininfo3 = "That's a " + str(percentageGain3) + "% gain!"
-
-    returnInfo = {'startInfo1': startInfo1,
-                  'buyInfo1': buyInfo1,
-                  'endStockInfo1': endStockInfo1,
-                  'endInvestmentInfo1': endInvestmentInfo1,
-                  'investmentGainInfo1': investmentGainInfo1,
-                  'percentageGaininfo1': percentageGaininfo1}
-
-    return (returnInfo)
-
-
 
 def stockPlotter(firstStockProg, secondStockProg, thirdStockProg, startDate2, endDate2): #Implement multiple names (name1, name2 and name3?)
     stockName1 = yf.Ticker(firstStockProg)
