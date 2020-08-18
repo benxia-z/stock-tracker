046d538901941a2163100665bf026802eb133707
Benjamin Xia <bzijiexia@gmail.com>
2020-08-17 23:47:55 -0500

    site plot features

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,14 @@ from flask import render_template, make_response, request, redirect, url_for
 from app import app
 from flask_bootstrap import Bootstrap
 import base64
+import json
+# import plotly.plotly as py
+from plotly import graph_objs as go
+from plotly.io import to_html
+import plotly
+import plotly.express as px
+
+import numpy as np
 
 # import MathPart as mp
 from app.forms import StockForm
@@ -12,15 +20,14 @@ from app.forms import StockForm
 def index():
     form = StockForm()
     if form.validate_on_submit():
-        stock = form.stock.data
-        init_invest = form.init_invest.data
-        start_date = form.start_date.data
-        end_date = form.end_date.data
+        redirect(url_for('plot'))
 
-        redirect(url_for('index'))
-
-    return render_template('index.html', form=form, user='Arjun')
+    return render_template('index.html', form=form)
 
 @app.route('/plot')
 def plot():
-    return render_template()
+    fig = px.line(x=["a","b","c"], y=[1,3,2], title="sample figure")
+
+    figdiv = to_html(fig, full_html=False, include_plotlyjs=False)
+
+    return render_template('plot.html', figdiv=figdiv)
